6e51e71eb3c67f0f5062801011b9f0e44d8dd460
Beta-Naz <sabitovnazir6@gmail.com>
2026-05-30 20:16:21 +0500

    Разработанный классы общего-пользования. Sabitov

---
+++ TaskManager_Sabitov/Classes/Notification.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TaskManager_Sabitov.Classes
+{
+    public class Notification
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if(PropertyChanged != null) 
+            { 
+                PropertyChanged(this, new PropertyChangedEventArgs(prop)); 
+            }
+        }
+    }
+}
