44f4c3d4d7b444d9cff61e9eb2799603aa4baaff
kenn1d <makarkibanov220@gmail.com>
2026-05-30 12:12:15 +0500

    Разработанная главная страница. Sabitov

---
+++ TaskManager_Sabitov/View/Main.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace TaskManager_Sabitov.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main(Object context)
+        {
+            InitializeComponent();
+            DataContext = context;
+        }
+    }
+}
