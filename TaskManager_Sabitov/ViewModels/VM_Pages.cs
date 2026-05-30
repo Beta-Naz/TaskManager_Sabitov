1c57d2f269cb77597c65caf158578379ed944b0f
Beta-Naz <sabitovnazir6@gmail.com>
2026-05-30 21:49:26 +0500

    Разработанная модель представления главного окна. Sabitov

--- TaskManager_Sabitov/ViewModels/VM_Pages.cs
+++ TaskManager_Sabitov/ViewModels/VM_Pages.cs
@@ -5,10 +5,12 @@ namespace TaskManager_Sabitov.ViewModels
     public class VM_Pages : Notification 
     {
         public VM_Tasks VMTasks = new VM_Tasks();
+
         public VM_Pages()
         {
             MainWindow.Instance.OpenPage(new View.Main(VMTasks));
         }
+
         public RealyCommand OnClose
         {
             get
